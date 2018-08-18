6b6afa7dc6863b6887dc5189da6abe0099c1e9f0
mattrudin <matthias.rudin@live.com>
2018-08-18 16:54:32 +0200

    Updated Project 5

--- Project5/flex.panels/src/App.js
+++ Project5/flex.panels/src/App.js
@@ -3,13 +3,29 @@ import './App.css';
 import Panels from './components/Panels/Panels';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Panels />
-      </div>
-    );
-  }
+  	constructor(props) {
+		super(props);
+		this.state = {
+			panelNames: [`panel panel1`,`panel panel2`,`panel panel3`,`panel panel4`,`panel panel5`],
+			panelText: [[`Hey`, `Let's`, `Dance`], [`Give`,`Take`,`Receive`], [`Experience`,`It`,`Today`], [`Give`,`All`,`You can`], [`Life`,`In`,`Motion`]]
+		};
+
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		console.log('hello');
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Panels onClick={this.handleClick}
+						panelNames={this.state.panelNames} 
+						panelText={this.state.panelText} />
+			</div>
+		);
+  	}
 }
 
 export default App;
--- Project5/flex.panels/src/components/Panels/Panels.js
+++ Project5/flex.panels/src/components/Panels/Panels.js
@@ -2,21 +2,17 @@ import React from 'react';
 import './Panels.css';
 import Panel from '../Panel/Panel';
 
-class Panels extends React.Component{
-	constructor(props) {
-		super(props);
-		this.state = {
-			panelNames: [`panel panel1`,`panel panel2`,`panel panel3`,`panel panel4`,`panel panel5`],
-			panelText: [[`Hey`, `Let's`, `Dance`], [`Give`,`Take`,`Receive`], [`Experience`,`It`,`Today`], [`Give`,`All`,`You can`], [`Life`,`In`,`Motion`]]
-		};
-	}
-	render() {
-		return(
-			<div className="panels">
-				{this.state.panelNames.map((index, num) => <Panel className={index} panelText={this.state.panelText[num]}/>)}
-			</div>
-		);
-	}
-}
+const Panels = (props) => {
+	const panelNames = props.panelNames;
+	const panelText = props.panelText;
+	return(
+		<div className="panels">
+			{panelNames.map((index, num) => <Panel 	onClick={props.onClick}
+													className={index} 
+													key={num} 
+													panelText={panelText[num]} />)}
+	    </div>
+	);
+};
 
 export default Panels;
