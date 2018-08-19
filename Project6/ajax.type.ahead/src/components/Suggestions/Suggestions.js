c93249115e1b141e6d0b26884d545aa8abe9a2c2
mattrudin <matthias.rudin@live.com>
2018-08-19 12:26:32 +0200

    Updated readme.md; react setup started

---
+++ Project6/ajax.type.ahead/src/components/Suggestions/Suggestions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Suggestions.css';
+
+const Suggestions = (props) => {
+	return(
+		<div className="suggestions">
+	    	<ul class="suggestions">
+		      <li>Filter for a city</li>
+		      <li>or a state</li>
+		    </ul>
+	    </div>
+	);
+};
+
+export default Suggestions;
