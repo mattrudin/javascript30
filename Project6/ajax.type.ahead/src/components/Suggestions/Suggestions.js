805556bc5a509394abecfedfbeb2dae5c8c83a48
mattrudin <matthias.rudin@live.com>
2018-08-20 06:17:21 +0200

    Updated components; Input does not work yet

--- Project6/ajax.type.ahead/src/components/Suggestions/Suggestions.js
+++ Project6/ajax.type.ahead/src/components/Suggestions/Suggestions.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Suggestions.css';
 
 const Suggestions = (props) => {
+	const html = props.html;
+	const Placeholder= () => {
+		return(
+			<div>
+				<li>Filter for a city</li>
+			    <li>or a state</li>
+		    </div>
+		)
+	}
+
 	return(
 		<div className="suggestions">
-	    	<ul class="suggestions">
-		      <li>Filter for a city</li>
-		      <li>or a state</li>
+	    	<ul className="suggestions">
+		      {html ? html : <Placeholder/>}
 		    </ul>
 	    </div>
 	);
