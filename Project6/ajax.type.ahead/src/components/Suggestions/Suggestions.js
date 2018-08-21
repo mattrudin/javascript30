f06db93cb562cf4ff24ea68788570956d5d02ae1
mattrudin <matthias.rudin@live.com>
2018-08-21 12:49:57 +0200

    Text highlighting is missing

--- Project6/ajax.type.ahead/src/components/Suggestions/Suggestions.js
+++ Project6/ajax.type.ahead/src/components/Suggestions/Suggestions.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import './Suggestions.css';
 
+//may use highlighter for highlighting the search text? https://github.com/bvaughn/react-highlight-words
+
 const Suggestions = (props) => {
 	const matchArray = props.matchArray;
 	const searchInput = props.searchInput;
+	const numberWithCommas = props.numberWithCommas;
 	const regex = new RegExp(searchInput, 'gi');
 	const match = matchArray.map(place => {
-		const cityName = place.city.replace(regex,
-			<span className="hl">{searchInput}</span>);
-		const stateName = place.state.replace(regex,
-			<span className="hl">{searchInput}</span>);
+		const cityName = place.city.replace(regex, searchInput);
+		const stateName = place.state.replace(regex, searchInput);
 		return(
-			<div key={`${place.city}${place.state}`}>
-				<li>
-					<span className="name">{place.city}, {place.state}</span>
-					<span className="population">{place.population}</span>
+				<li key={`${place.city}${place.state}`}>
+					<span className="name"><span className="hl">{cityName}</span>, <span className="hl">{stateName}</span></span>
+					<span className="population">{numberWithCommas(place.population)}</span>
 				</li>
-			</div>
 		)	
 	});
 
